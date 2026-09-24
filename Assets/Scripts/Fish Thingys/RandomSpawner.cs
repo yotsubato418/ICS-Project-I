5d3dfd10a3813b43fb2b26b659653d41743a1d58
JosephL <xintongliumail@gmail.com>
2026-09-24 19:40:53 -0400

    Fish update follows camera dropping movement.

--- Assets/Scripts/Fish Thingys/RandomSpawner.cs
+++ Assets/Scripts/Fish Thingys/RandomSpawner.cs
@@ -8,13 +8,16 @@ public class RandomSpawner : MonoBehaviour
     public float maxX = 8f;
     public float minY = -6f;
     public float maxY = 1f;
+    Camera mainCamera;
     void Start()
     {
+        mainCamera = Camera.main;
         InvokeRepeating(nameof(SpawnFish), 0f, spawnInterval);
     }
     void SpawnFish()
     {
-        Vector3 randomPosition = new Vector3(Random.Range(minX, maxX), Random.Range(minY, maxY), 0f);
+        float cameraY = mainCamera.transform.position.y;
+        Vector3 randomPosition = new Vector3(Random.Range(minX, maxX), cameraY + Random.Range(minY, maxY), 0f);
         Instantiate(ItemPrefab, randomPosition, Quaternion.identity);
     }
 }
