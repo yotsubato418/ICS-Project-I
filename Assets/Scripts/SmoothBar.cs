85b368203d8638628c59c107d6677d88872d88cd
JosephL <xintongliumail@gmail.com>
2026-09-26 00:01:11 -0400

    Modified more efficient code, debugged.

--- Assets/Scripts/SmoothBar.cs
+++ Assets/Scripts/SmoothBar.cs
@@ -1,24 +1,22 @@
 using UnityEngine;
-using UnityEngine.UI;
 
 public class SmoothBar : MonoBehaviour
 {
-    public Transform fillTransform;
     public float maxValue = 100f;
     public float currentValue = 100f;
     public float smoothSpeed = 2f;
     float fullWidth;
     void Start()
     {
-        fullWidth = fillTransform.localScale.x;
+        fullWidth = transform.localScale.x;
     }
 
     // Update is called once per frame
     void Update()
     {
         float targetFill = fullWidth * (currentValue / maxValue);
-        float newScale = Mathf.MoveTowards(fillTransform.localScale.x, targetFill, smoothSpeed * Time.deltaTime);
-        fillTransform.localScale = new Vector3(newScale, fillTransform.localScale.y, fillTransform.localScale.z);
+        float newScale = Mathf.MoveTowards(transform.localScale.x, targetFill, smoothSpeed * Time.deltaTime);
+        transform.localScale = new Vector3(newScale, transform.localScale.y, transform.localScale.z);
     }
 
     public void SetValue(float newValue)
